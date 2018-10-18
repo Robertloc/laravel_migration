4a8a307a0d45136c6c840c8670b5b8d697f27a3c
seniessl <stephanie.niessl@gmail.com>
2018-10-18 11:46:46 +0200

    fixed migration structure

--- database/migrations/2018_10_16_140429_questions.php
+++ database/migrations/2018_10_16_140429_questions.php
@@ -16,9 +16,9 @@ class Questions extends Migration
         //
     Schema::create('questions', function (Blueprint $table) { // schema for table 'questions'
         $table->increments('id');                         // add column 'id' that will be AI PK
-        $table->unsignedInteger('user_id')->nullable()->change();                         // create string column 'user id'
-        $table->string('title', 127)->nullable()->change();                           // create a string column 'name'
-        $table->string('text')->nullable()->change();                // create a string column 'email' which will be unique
+        $table->unsignedInteger('user_id')->nullable();                         // create string column 'user id'
+        $table->string('title', 127)->nullable();                           // create a string column 'name'
+        $table->string('text')->nullable();                // create a string column 'email' which will be unique
         $table->timestamps();                             // add common columns 'created_at' and 'updated_at'
     });
 
--- database/migrations/2018_10_18_092852_answers_nullable_columns.php
+++ database/migrations/2018_10_18_092852_answers_nullable_columns.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddTitle extends Migration
+class AnswersNullableColumns extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,10 @@ class AddTitle extends Migration
      */
     public function up()
     {
-        Schema::table('answers', function(Blueprint $table) {
-            $table->string('title', 127)->nullable()->after('question_id');
+        //
+        Schema::table('answers', function (Blueprint $table) {
+            //
+            $table->text('text')->nullable()->change();
         });
     }
 
@@ -25,8 +27,10 @@ class AddTitle extends Migration
      */
     public function down()
     {
-        Schema::table('answers', function(Blueprint $table) {
-            $table->dropColumn('title');
+        //
+        Schema::table('answers', function (Blueprint $table) {
+            //
+            $table->text('text')->nullable(false)->change();
         });
     }
 }
